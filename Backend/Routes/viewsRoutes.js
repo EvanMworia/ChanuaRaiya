d3abda1aa8539827bb2cfefa9b7cd4d8084079e3
EvanMworia <evannji99@gmail.com>
2025-03-05 18:45:12 +0300

    Chore: Added a getAllViewsFromTopic endpoint with its controllers, routes and stored procedures

--- Backend/Routes/viewsRoutes.js
+++ Backend/Routes/viewsRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { shareView, getAllViews, getViewById, deleteView } = require('../Controllers/viewsController');
+const { shareView, getAllViews, getViewById, deleteView, getViewsOnATopic } = require('../Controllers/viewsController');
 const viewsRouter = express.Router();
 
 viewsRouter.post('/create-view', shareView);
 viewsRouter.get('/get-all-views', getAllViews);
 viewsRouter.get('/get-view/:id', getViewById);
+viewsRouter.get('/topic-views/:id', getViewsOnATopic);
 viewsRouter.delete('/delete-view/:id', deleteView);
 
 module.exports = viewsRouter;
